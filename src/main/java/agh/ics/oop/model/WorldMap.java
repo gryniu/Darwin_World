585ae8fe734ab91b4dea7b9b7bed17dd3e6e556d
maciejjanusz <maciekjanusz.2005@gmail.com>
2026-01-07 13:54:18 +0100

    podstawowe klasy v1

---
+++ src/main/java/agh/ics/oop/model/WorldMap.java
@@ -0,0 +1,57 @@
+package agh.ics.oop.model;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * The interface responsible for interacting with the map of the world.
+ * Assumes that Vector2d and MoveDirection classes are defined.
+ *
+ * @author apohllo, idzik
+ */
+public interface WorldMap extends MoveValidator {
+
+    /**
+     * Place a new animal on the map.
+     *
+     * @param animal The animal to be placed on the map.
+     * @return True if the animal was placed. The rules for valid placement are the same as for movement.
+     */
+    void place(Animal animal);
+
+    /**
+     * Moves an animal (if it is present on the map) according to specified direction.
+     * If the move is not possible, this method has no effect.
+     */
+    void move(Animal animal, MoveDirection direction);
+
+    /**
+     * Return true if given position on the map is occupied. Should not be
+     * confused with canMoveTo since there might be empty positions where the animal
+     * cannot move.
+     *
+     * @param position Position to check.
+     * @return True if the position is occupied.
+     */
+    boolean isOccupied(Vector2d position);
+
+    /**
+     * Return an animal at a given position.
+     *
+     * @param position The position of the animal.
+     * @return animal or null if the position is not occupied.
+     */
+    Optional<WorldElement> objectAt(Vector2d position);
+
+    List<WorldElement> getElements();
+
+    void addSubscriber(MapChangeListener observer);
+    void removeSubscriber(MapChangeListener observer);
+    void mapChanged(String message);
+
+    UUID getId();
+
+    Boundary getCurrentBounds();
+    List<Animal> getAnimals();
+}
