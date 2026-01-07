0c02d47f254af0e6b17d3ad887126214fb3d0031
maciejjanusz <maciekjanusz.2005@gmail.com>
2026-01-07 14:02:06 +0100

    move validator usuniety

--- src/main/java/agh/ics/oop/model/WorldMap.java
+++ src/main/java/agh/ics/oop/model/WorldMap.java
@@ -10,7 +10,7 @@ import java.util.UUID;
  *
  * @author apohllo, idzik
  */
-public interface WorldMap extends MoveValidator {
+public interface WorldMap{
 
     /**
      * Place a new animal on the map.
@@ -24,7 +24,7 @@ public interface WorldMap extends MoveValidator {
      * Moves an animal (if it is present on the map) according to specified direction.
      * If the move is not possible, this method has no effect.
      */
-    void move(Animal animal, MoveDirection direction);
+    void move(Animal animal);
 
     /**
      * Return true if given position on the map is occupied. Should not be
