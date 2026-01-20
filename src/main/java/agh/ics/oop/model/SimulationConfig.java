0355c5282e06aaaf5ad58bea522727fa2d2944c4
Bartosz Gryń <bartoszgryn@Bartoszs-MacBook-Air-2.local>
2026-01-20 02:03:03 +0100

    refactoring mainPresentera

--- src/main/java/agh/ics/oop/model/SimulationConfig.java
+++ src/main/java/agh/ics/oop/model/SimulationConfig.java
@@ -1,8 +1,5 @@
 package agh.ics.oop.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SimulationConfig {
     public final boolean isSeasonal;
     public final boolean isAnimalAdd;
@@ -23,7 +20,7 @@ public class SimulationConfig {
     public final int minTemperature;
     public final int distanceRequiredToHeat;
 
-    private SimulationConfig(Builder b) {
+    private SimulationConfig(Builder b) throws WrongFieldStateException {
         this.isSeasonal = b.isSeasonal;
         this.isAnimalAdd = b.isAnimalAdd;
         this.mapWidth = b.mapWidth;
@@ -42,6 +39,7 @@ public class SimulationConfig {
         this.seasonLength = b.seasonLength;
         this.minTemperature = b.minTemperature;
         this.distanceRequiredToHeat = b.distanceRequiredToHeat;
+        SimulationConfigValidator.validate(this);
     }
 
     public static Builder builder() {
@@ -88,72 +86,9 @@ public class SimulationConfig {
         public Builder distanceRequiredToHeat(int v){ distanceRequiredToHeat = v; return this; }
 
         public SimulationConfig build() throws WrongFieldStateException {
-            validate();
             return new SimulationConfig(this);
         }
 
-        private void validate() throws WrongFieldStateException {
-            List<String> errors = new ArrayList<>();
-
-            // todo: porobic ograniczenia na pola
-            if (mapWidth <= 1 || mapWidth > 100)
-                errors.add("mapWidth: szerokość mapy musi być > 1 i ≤ 100");
-
-            if (mapHeight <= 1 || mapHeight > 100)
-                errors.add("mapHeight: wysokość mapy musi być > 1 i ≤ 100");
-
-            if (startPlantCount < 0)
-                errors.add("startPlantCount: startowa liczba roślin musi być >= 0");
-
-            if (energyFromPlant <= 0)
-                errors.add("energyFromPlant: energia z rośliny musi być > 0");
-
-            if (plantsPerDay < 0)
-                errors.add("plantsPerDay: liczba roślin na dzień nie może być ujemna");
-
-            if (startAnimalCount <= 0)
-                errors.add("startAnimalCount: liczba startowych zwierząt musi być > 0");
-
-            if (startAnimalEnergy <= 0)
-                errors.add("startAnimalEnergy: energia startowa zwierząt musi być > 0");
-
-            if (energyLossPerDay < 0)
-                errors.add("energyLossPerDay: dzienna utrata energii nie może być ujemna");
-
-            if (energyToReproduce <= 0)
-                errors.add("energyToFeed: energia potrzebna do rozmnażania musi być > 0");
-
-            if (energyToKid <= 0 || energyToKid > energyToReproduce)
-                errors.add("energyToKid: energia dla potomka musi być > 0 i ≤ energii do rozmnażania");
-
-            if (minMutations < 0)
-                errors.add("minMutations: minimalna liczba mutacji nie może być ujemna");
-
-            if (maxMutations < minMutations || maxMutations > genomeLength)
-                errors.add("maxMutations: musi być ≥ minMutations i ≤ genomeLength");
-
-            if (genomeLength <= 0)
-                errors.add("genomeLength: długość genomu musi być > 0");
-
-            if (isSeasonal) {
-                if (seasonLength < 0)
-                    errors.add("seasonLength: długość sezonu nie może być ujemna");
-
-                if (minTemperature > 30)
-                    errors.add("minTemperature: minimalna temperatura nie może być większa niż 30");
-
-                if (distanceRequiredToHeat < 0 || distanceRequiredToHeat > Math.max(mapWidth, mapHeight))
-                    errors.add("distanceRequiredToHeat: odległość poza zakresem mapy");
-            } else {
-                seasonLength = 0;
-                minTemperature = 0;
-                distanceRequiredToHeat = 0;
-            }
-
-            if (!errors.isEmpty()) {
-                throw new WrongFieldStateException(errors);
-            }
-        }
 
     }
 }
